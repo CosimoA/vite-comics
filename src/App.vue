6c8cecb07527c6062287f63a4e3025f54148bb51
CosimoA <cosimo.archip@gmail.com>
2023-12-21 18:41:38 +0100

    FEAT: add header & main components

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <script>
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from './components/AppMain.vue'
+
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppMain,
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables.scss' as *;
-
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;900&display=swap');
 </style>
